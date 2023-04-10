5573180c0eef4b78cd9b4aedee9d6f7544cadb30
Joaquin <desarrollos@yellowpatito.com>
2023-04-10 11:19:41 -0300

    fix price controller

--- controllers/precioController.js
+++ controllers/precioController.js
@@ -6,32 +6,50 @@ const precioController = {
 
     newPrecio: async (req, res) => {
         try {
-            const precioExists = await Precio.findOne({ idUser: req.body.idUser });
-            if (precioExists) {
-                if (req.body.precio && req.body.precio.trim().length !== 0) {
-                    precioExists.precio = req.body.precio;
-                }
-                var id = precioExists._id
-                await User.findOneAndUpdate({ _id: req.body.idUser }, { $set: { precioData: id } }, { new: true })
-
-                await precioExists.save();
-                return res.status(200).send({ message: 'Precio updated successfully' });
-            } else {
-                const newPrecio = new Precio({
-                    precio: req.body.precio,
-                    idUser: req.body.idUser
-                });
-
-                var id = newPrecio._id
-                await User.findOneAndUpdate({ _id: req.body.idUser }, { $set: { precioData: id } }, { new: true })
-                await newPrecio.save();
-                return res.status(200).send({ message: 'Precio created successfully' });
+          const precioExists = await Precio.findOne({ idUser: req.body.idUser });
+      
+          if (precioExists) {
+            if (req.body.precio && req.body.precio.length > 0 && req.body.precio[0].pais && req.body.precio[0].pais.length > 0) {
+              precioExists.precio[0].pais = req.body.precio[0].pais;
             }
+            if (req.body.precio && req.body.precio.length > 0 && req.body.precio[0].canal && req.body.precio[0].canal.length > 0) {
+              precioExists.precio[0].canal = req.body.precio[0].canal;
+            }
+            if (req.body.precio && req.body.precio.length > 0 && req.body.precio[0].year && req.body.precio[0].year.length > 0) {
+              precioExists.precio[0].year = req.body.precio[0].year;
+            }
+            if (req.body.precio && req.body.precio.length > 0 && req.body.precio[0].producto && req.body.precio[0].producto.length > 0 && req.body.precio[0].producto[0].id && req.body.precio[0].producto[0].id.length > 0) {
+              precioExists.precio[0].producto[0].id = req.body.precio[0].producto[0].id;
+            }
+            if (req.body.precio && req.body.precio.length > 0 && req.body.precio[0].producto && req.body.precio[0].producto.length > 0 && req.body.precio[0].producto[0].product && req.body.precio[0].producto[0].product.length > 0) {
+              precioExists.precio[0].producto[0].product = req.body.precio[0].producto[0].product;
+            }
+            if (req.body.precio && req.body.precio.length > 0 && req.body.precio[0].producto && req.body.precio[0].producto.length > 0 && req.body.precio[0].producto[0].months && req.body.precio[0].producto[0].months.length > 0) {
+              precioExists.precio[0].producto[0].months = req.body.precio[0].producto[0].months;
+            }
+      
+            var id = precioExists._id;
+            await User.findOneAndUpdate({ _id: req.body.idUser }, { $set: { precioData: id } }, { new: true });
+            await precioExists.save();
+      
+            return res.status(200).send({ message: 'Precio updated successfully' });
+          } else {
+            const newPrecio = new Precio({
+              precio: req.body.precio,
+              idUser: req.body.idUser
+            });
+      
+            var id = newPrecio._id;
+            await User.findOneAndUpdate({ _id: req.body.idUser }, { $set: { precioData: id } }, { new: true });
+            await newPrecio.save();
+      
+            return res.status(200).send({ message: 'Precio created successfully' });
+          }
         } catch (error) {
-            return res.status(500).send({ error: error.message });
+          return res.status(500).send({ error: error.message });
         }
-
-    },
+      },
+      
 
     eachPrecio: (req, res) => {
         const { id } = req.params
