2f23301b5391dae11f4000e16d80740633f83f30
Joaquin <desarrollos@yellowpatito.com>
2023-05-26 11:26:33 -0300

    limit data in server

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const router = require('./routes/index')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb'}));
 
 app.use('/api', router)
 
