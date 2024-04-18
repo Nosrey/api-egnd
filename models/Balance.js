a61d511a05d16a2d5e5f837c7ac1126825542f3d
YellowPatito <123901671+YellowPatito@users.noreply.github.com>
2024-04-18 12:28:41 -0300

    Update Balance.js

--- models/Balance.js
+++ models/Balance.js
@@ -7,13 +7,17 @@ const BalanceSchema = new mongoose.Schema({
     creditosFiscales: { type: String },
     bienesDeCambio: { type: String },
     bienesDeUso: { type: String },
+    totActivo: { type: String },
     deudasComerciales: { type: String },
     deudasFiscales: { type: String },
     deudasFinancieras: { type: String },
     otrasDeudas: { type: String },
+    totPasivo: { type: String },
     equity: { type: String },
     resultadosNoAsignados: { type: String },
     resultadosDelEjercicio: { type: String },
+    totPatNeto: { type: String },
+    totPnYPasivo: { type: String },
     idUser: [{ type: mongoose.Schema.ObjectId, ref: "user" }],
 
 })
